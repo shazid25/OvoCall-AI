3e493458a8791065643f249de0150098fa0ef9fb
shazid25 <ishazid57@gmail.com>
2025-12-25 22:48:37 +0600

    social authentication added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,18 @@
-"use client";
-import { authClient } from "@/lib/auth-client";
-import { useState } from "react";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import { HomeView } from "@/modules/home/ui/views/home-view";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-   const {data: session} = authClient.useSession();
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onSubmit =() =>{
-    authClient.signUp.email({
-      email,
-      password,
-      name,
-    },{
-      onError:()=>{
-        window.alert("Something went wrong!");
-      },
-      onSuccess: ()=>{
-        window.alert("User created successfully!");
-      }
-    });
-  };
-
-  if(session){
-    return(
-      <div className="flex flex-col p-4 gap-y-4">
-        <p>Logged in as {session.user.name}</p>
-        <button onClick={()=>authClient.signOut()}>Sign out</button>
-      </div>
-    )
+  if (!session) {
+    redirect("/sign-in");
   }
 
-  return (
-    <div className="p-4 flex flex-col gap-y-3">
-      <Input
-        placeholder="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Input
-        placeholder="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button onClick={onSubmit}>
-        Create user
-      </Button>
-    </div>
-  );
-}
+  return <HomeView />
+};
+
+export default Page;

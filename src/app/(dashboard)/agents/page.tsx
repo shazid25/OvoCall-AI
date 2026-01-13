e3a300ec861090e9c89576d9fa6aa661057809a3
shazid25 <ishazid57@gmail.com>
2026-01-13 21:39:24 +0600

    pagination added

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,15 +1,23 @@
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { Suspense } from "react";
-// import { AgentsView, AgentsViewError, AgentsViewLoading } from "@/modules/agents/server/ui/views/agents-views";
 import { ErrorBoundary } from "react-error-boundary";
 import { AgentsView, AgentsViewError, AgentsViewLoading } from "@/modules/agents/ui/views/agents-views";
 import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import type { SearchParams } from "nuqs";
+import { loadSearchParams } from "@/modules/agents/params";
 
-const Page = async () => {
+
+interface Props{
+  searchParams: Promise<SearchParams>;
+};
+
+const Page = async ({ searchParams }: Props) => {
+   const filters = await loadSearchParams(searchParams);
+
     const session = await auth.api.getSession({
         headers: await headers(),
       });
@@ -20,7 +28,9 @@ const Page = async () => {
 
 
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({}));
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+        ...filters,
+    }));
 
     return (
         <>
--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -1,17 +1,28 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { PlusIcon } from "lucide-react";
+import { PlusIcon, XCircleIcon } from "lucide-react";
 import { NewAgentDialog } from "./new-agent-dialog";
 import { useState } from "react";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { AgentsSearchFilter } from "./agents-search-filters";
+import { DEFAULT_PAGE } from "@/constants";
 
 
 export const AgentsListHeader = () => {
 
     const [filters, setFilters] = useAgentsFilters();
     const [ isDialogOpen, setIsDialogOpen] = useState(false);
+    
+    const isAnyFilterModified =  !!filters.search;
+
+    const onClearFilters = () => {
+        setFilters({
+            search: "",
+            page:  DEFAULT_PAGE,
+        }); 
+    }
+
     return (
        <>
        <NewAgentDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
@@ -27,6 +38,13 @@ export const AgentsListHeader = () => {
      
          <div className="flex items-center gap-x-2 p-1">
            <AgentsSearchFilter/>
+              {isAnyFilterModified && (
+                <Button variant="outline" size="sm" onClick={onClearFilters}>
+                    <XCircleIcon />
+                    Clear
+                </Button>
+                )}
+
          </div>
 
 

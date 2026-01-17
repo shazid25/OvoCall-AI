ea3be33d8becd90e26f4a81a81ec1782cdb6986a
shazid25 <ishazid57@gmail.com>
2026-01-17 15:36:12 +0600

    Fixed command select filter reset

--- src/modules/meetings/ui/components/meeting-form.tsx
+++ src/modules/meetings/ui/components/meeting-form.tsx
@@ -39,21 +39,21 @@ export const MeetingForm = ({
   //const router = useRouter();
   const queryClient = useQueryClient();
 
-  const [ openNewAgentDialog, setOpenNewAgentDialog ] = useState(false);
-  const [ agentSearch, setAgentSearch ] = useState("");
+  const [openNewAgentDialog, setOpenNewAgentDialog] = useState(false);
+  const [agentSearch, setAgentSearch] = useState("");
 
   const agents = useQuery(
     trpc.agents.getMany.queryOptions({
-      pageSize:  100,
-      search: agentSearch ,
+      pageSize: 100,
+      search: agentSearch,
     })
   )
 
 
   const createMeeting = useMutation(
     trpc.meetings.create.mutationOptions({
-      onSuccess: async(data) => {
-      await  queryClient.invalidateQueries(
+      onSuccess: async (data) => {
+        await queryClient.invalidateQueries(
           trpc.meetings.getMany.queryOptions({}),
         );
 
@@ -74,16 +74,16 @@ export const MeetingForm = ({
 
   //update meeting
 
-   const updateMeeting = useMutation(
+  const updateMeeting = useMutation(
     trpc.meetings.update.mutationOptions({
-      onSuccess: async() => {
-      await  queryClient.invalidateQueries(
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(
           trpc.meetings.getMany.queryOptions({}),
         );
-       
-        if(initialValues?.id){
+
+        if (initialValues?.id) {
           await queryClient.invalidateQueries(
-            trpc.meetings.getOne.queryOptions({id: initialValues.id}),
+            trpc.meetings.getOne.queryOptions({ id: initialValues.id }),
           );
         }
         onSuccess?.();
@@ -97,7 +97,7 @@ export const MeetingForm = ({
     }),
   );
 
-  const form = useForm<z.infer<typeof meetingsInsertSchema >>({
+  const form = useForm<z.infer<typeof meetingsInsertSchema>>({
     resolver: zodResolver(meetingsInsertSchema),
     defaultValues: {
       name: initialValues?.name ?? "",
@@ -119,97 +119,97 @@ export const MeetingForm = ({
   return (
 
     <>
-    <NewAgentDialog open={openNewAgentDialog} onOpenChange={setOpenNewAgentDialog} />
+      <NewAgentDialog open={openNewAgentDialog} onOpenChange={setOpenNewAgentDialog} />
 
-    <Form {...form}>
-      <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
-        <GeneratedAvatar
-          seed={form.watch("name")}
-          variant="botttsNeutral"
-          className="border size-16"
-        />
+      <Form {...form}>
+        <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+          <GeneratedAvatar
+            seed={form.watch("name")}
+            variant="botttsNeutral"
+            className="border size-16"
+          />
 
-        <FormField
-          name="name"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input {...field } placeholder="e.g. Math Instructor" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-
-
-        <FormField
-  name="agentId"
-  control={form.control}
-  render={({ field }) => (
-    <FormItem>
-      <FormLabel>Agent</FormLabel>
-      <FormControl>
-        <CommandSelect
-          options={(agents.data?.items ?? []).map((agent) => ({
-            id: agent.id,
-            value: agent.id,
-            children: (
-              <div className="flex items-center gap-x-2">
-                <GeneratedAvatar
-                  seed={agent.name}
-                  variant="botttsNeutral"
-                  className="border size-6"
-                />
-                <span>{agent.name}</span>
-              </div> 
-            ),
-          }))}
-          onSelect={field.onChange}
-          onSearch={setAgentSearch}
-          value={field.value}
-          placeholder="Select an agent"
-        /> 
-      </FormControl>
-      
-       <FormDescription>
-        Not found what you&appos;re looking for?{" "}
-        <Button
-          type="button"
-          className=" hover:underline"
-          onClick={() => setOpenNewAgentDialog(true)}
-        >
-          Create a new agent
-        </Button>
-       </FormDescription>
-
-      <FormMessage />
-    </FormItem>
-  )}
-/>
+          <FormField
+            name="name"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="e.g. Math Instructor" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
 
-        
 
-        <div className="flex justify-between gap-x-2">
-          {onCancel && (
-            <Button
-              variant="ghost"
-              disabled={isPending}
-              type="button"
-              onClick={() => onCancel()}
-            >
-              Cancel
+          <FormField
+            name="agentId"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Agent</FormLabel>
+                <FormControl>
+                  <CommandSelect
+                    options={(agents.data?.items ?? []).map((agent) => ({
+                      id: agent.id,
+                      value: agent.id,
+                      children: (
+                        <div className="flex items-center gap-x-2">
+                          <GeneratedAvatar
+                            seed={agent.name}
+                            variant="botttsNeutral"
+                            className="border size-6"
+                          />
+                          <span>{agent.name}</span>
+                        </div>
+                      ),
+                    }))}
+                    onSelect={field.onChange}
+                    onSearch={setAgentSearch}
+                    value={field.value}
+                    placeholder="Select an agent"
+                  />
+                </FormControl>
+
+                <FormDescription>
+                  Not found what you&appos;re looking for?{" "}
+                  <Button
+                    type="button"
+                    className=" hover:underline"
+                    onClick={() => setOpenNewAgentDialog(true)}
+                  >
+                    Create a new agent
+                  </Button>
+                </FormDescription>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+
+
+
+          <div className="flex justify-between gap-x-2">
+            {onCancel && (
+              <Button
+                variant="ghost"
+                disabled={isPending}
+                type="button"
+                onClick={() => onCancel()}
+              >
+                Cancel
+              </Button>
+            )}
+            <Button disabled={isPending} type="submit">
+              {isEdit ? "Update" : "Create"}
             </Button>
-          )}
-          <Button disabled={isPending} type="submit">
-            {isEdit ? "Update" : "Create"}
-          </Button>
-        </div>
-      </form>
-    </Form>
+          </div>
+        </form>
+      </Form>
     </>
   )
 }

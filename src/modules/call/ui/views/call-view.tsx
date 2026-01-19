3d7a724053df8aff69d3dad7119e7045adfe7217
shazid25 <ishazid57@gmail.com>
2026-01-19 20:11:01 +0600

    working on live calling feature

--- src/modules/call/ui/views/call-view.tsx
+++ src/modules/call/ui/views/call-view.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { ErrorState } from "@/components/error-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
@@ -11,6 +12,17 @@ export const CallView = ({
 }: Props) => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.meetings.getOne.queryOptions({ id: meetingId }));
+
+    if (data.status === "completed") {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <ErrorState
+                title="Meeting has ended"
+                description="You can no longer join this meeting"
+            />
+        </div>
+    );
+}
 
     return(
         <div>
